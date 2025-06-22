f47986b4bf6c483c799dcaddffb348ef8acaceba
Avisek Ray <biisal.int@gmail.com>
2025-06-22 15:50:31 +0530

    Fixes typo, adds path to profile, and reloads shell after install.

--- install.sh
+++ install.sh
@@ -4,14 +4,26 @@ set -e
 BINARY_NAME="godo"
 BINARY_URL="https://github.com/biisal/godo/releases/download/v22.6.25/godo"
 INSTALL_DIR="$HOME/.local/bin"
+COMMNET="# run_godo shortcut"
 
 mkdir -p "$INSTALL_DIR"
-echo "Making you more focus on what you want to do..."
+echo "Making you more focused on what you want to do..."
 curl -sSL "$BINARY_URL" -o "$INSTALL_DIR/$BINARY_NAME"
 chmod +x "$INSTALL_DIR/$BINARY_NAME"
-echo "Almost reday....."
+echo "Almost ready..."
 
-COMMNET="# run_godo shortcut"
+LINE='export PATH="$HOME/.local/bin:$PATH"'
+if command -v zsh >/dev/null 2>&1; then
+    PROFILE="$HOME/.zshrc"
+else
+    PROFILE="$HOME/.bashrc"
+fi
+
+if ! grep -qxF "$LINE" "$PROFILE"; then
+    echo "$LINE" >> "$PROFILE"
+    echo "🔧 Added ~/.local/bin to PATH in $PROFILE"
+fi
+
 if command -v zsh >/dev/null 2>&1; then
 	if ! grep -q "$COMMNET" ~/.zshrc; then
 	cat >> ~/.zshrc <<EOL
@@ -33,4 +45,8 @@ bind '"\C-g":"godo\n"'
 EOL
 fi
 
-echo "Completed... Now close and open new terminal and Press Ctrl+g ..tq [laal dil]"
+echo "Reloading shell..."
+exec "$SHELL"
+
+
+echo "Done. Press Ctrl+g to start.... [laal dil]"
